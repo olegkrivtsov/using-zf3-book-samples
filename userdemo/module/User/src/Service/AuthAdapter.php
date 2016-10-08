a05184574e45e272371b8e952e2d4e0e63f0c0fa
Oleg Krivtsov <olegkrivtsov@gmail.com>
2016-10-08 21:43:55 +0600

    Removed dependency on zend-barcode

--- userdemo/module/User/src/Service/AuthAdapter.php
+++ userdemo/module/User/src/Service/AuthAdapter.php
@@ -7,8 +7,8 @@ use Zend\Crypt\Password\Bcrypt;
 use User\Entity\User;
 
 /**
- * Adapter used for authenticating user. It takes username and password on input
- * and checks the database if there is a user with such username (email) and password.
+ * Adapter used for authenticating user. It takes login and password on input
+ * and checks the database if there is a user with such login (email) and password.
  * If such user exists, the service returns its identity (email). The identity
  * is saved to session and can be retrieved later with Identity view helper provided
  * by ZF3.
