e3fb65a767c3de1f992ff660f118e3857866418b
Illia Kovalov <ikovalyov85@gmail.com>
2017-06-01 12:52:11 +0200

    fix Fatal error: Uncaught Zend\Session\Exception\RuntimeException: Session validation failed

--- src/Service/Factory/AuthenticationServiceFactory.php
+++ src/Service/Factory/AuthenticationServiceFactory.php
@@ -5,6 +5,7 @@ use Interop\Container\ContainerInterface;
 use Zend\Authentication\AuthenticationService;
 use Zend\ServiceManager\Exception\ServiceNotCreatedException;
 use Zend\ServiceManager\Factory\FactoryInterface;
+use Zend\Session\Exception\RuntimeException;
 use Zend\Session\SessionManager;
 use Zend\Authentication\Storage\Session as SessionStorage;
 use ProspectOne\UserModule\Service\AuthAdapter;
@@ -27,7 +28,12 @@ class AuthenticationServiceFactory implements FactoryInterface
     {
         /** @var SessionManager $sessionManager */
         $sessionManager = $container->get(SessionManager::class);
-        $sessionManager->start();
+        try {
+            $sessionManager->start();
+        } catch (RuntimeException $e) {
+            session_unset();
+            $sessionManager->start();
+        }
         try {
             $authStorage = new SessionStorage('Zend_Auth', 'session', $sessionManager);
         } catch (ServiceNotCreatedException $e) {
