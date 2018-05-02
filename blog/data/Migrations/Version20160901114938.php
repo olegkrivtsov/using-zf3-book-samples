e85ae7b27785b7e67eb6ce9f736ef35bd7fbf8e8
Oleg Krivtsov <olegkrivtsov@gmail.com>
2018-05-02 20:15:00 +0600

    Fixed: database migrations error #30

--- blog/data/Migrations/Version20160901114938.php
+++ blog/data/Migrations/Version20160901114938.php
@@ -27,17 +27,17 @@ class Version20160901114938 extends AbstractMigration
     {
         // Add index to post table
         $table = $schema->getTable('post');
-        $table->addIndex(['date_created'], 'date_created_index');
+        $table->addIndex(['date_created'], 'post_date_created_index');
         
         // Add index and foreign key to comment table
         $table = $schema->getTable('comment');
-        $table->addIndex(['post_id'], 'post_id_index');
+        $table->addIndex(['post_id'], 'comment_post_id_index');
         $table->addForeignKeyConstraint('post', ['post_id'], ['id'], [], 'comment_post_id_fk');
         
         // Add indexes and foreign keys to post_tag table
         $table = $schema->getTable('post_tag');
-        $table->addIndex(['post_id'], 'post_id_index');
-        $table->addIndex(['tag_id'], 'tag_id_index');
+        $table->addIndex(['post_id'], 'post_tag_post_id_index');
+        $table->addIndex(['tag_id'], 'post_tag_id_index');
         $table->addForeignKeyConstraint('post', ['post_id'], ['id'], [], 'post_tag_post_id_fk');
         $table->addForeignKeyConstraint('tag', ['tag_id'], ['id'], [], 'post_tag_tag_id_fk');
     }
@@ -50,14 +50,14 @@ class Version20160901114938 extends AbstractMigration
         $table = $schema->getTable('post_tag');
         $table->removeForeignKey('post_tag_post_id_fk');
         $table->removeForeignKey('post_tag_tag_id_fk');
-        $table->dropIndex('post_id_index');
-        $table->dropIndex('tag_id_index'); 
+        $table->dropIndex('post_tag_post_id_index');
+        $table->dropIndex('post_tag_tag_id_index'); 
         
         $table = $schema->getTable('comment');
-        $table->dropIndex('post_id_index'); 
+        $table->dropIndex('comment_post_id_index'); 
         $table->removeForeignKey('comment_post_id_fk');
         
         $table = $schema->getTable('post');
-        $table->dropIndex('date_created_index');       
+        $table->dropIndex('post_date_created_index');       
     }
 }
